b7ef2aa212e917b028963058a1aa4dda1284c9f3
FrancoBarberis <francobarberissic3@gmail.com>
2025-07-13 13:24:40 -0300

    script descartar

--- T-BURGER.LABS/js/index.js
+++ T-BURGER.LABS/js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Selecciona todos los botones de sumar y restar
   const botonesSumar = document.querySelectorAll(".btn-sumar");
   const botonesRestar = document.querySelectorAll(".btn-restar");
+  const botonDescartar = document.querySelector(".btn-descartar");
 
   // Agrega el event listener para los botones de sumar
   botonesSumar.forEach((boton) => {
@@ -19,5 +20,22 @@ document.addEventListener("DOMContentLoaded", () => {
         cantidad.textContent = parseInt(cantidad.textContent) - 1; // Decrementa la cantidad si es mayor a 0
       }
     });
+  });
+
+  // Agrega el event listener para el botón de descartar
+  botonDescartar.addEventListener("click", () => {
+    const cantidades = document.querySelectorAll(".cantidad"); // Selecciona todos los spans con la clase cantidad
+    let seDescarto = false; // Variable para verificar si se descartó alguna cantidad
+
+    cantidades.forEach((cantidad) => {
+      if (parseInt(cantidad.textContent) > 0) {
+        seDescarto = true; // Marca que se descartó al menos una cantidad
+        cantidad.textContent = 0; // Resetea la cantidad a 0
+      }
+    });
+
+    if (seDescarto) {
+      alert("Pedido descartado"); // Muestra el mensaje solo si se descartó algo
+    }
   });
 });
